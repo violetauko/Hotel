42ebb35b3070c952118b60e18a64dfb74fa771b6
violetauko <aukoviolet24@gmail.com>
2022-05-28 17:21:34 +0300

    add found hotel images

--- app/src/main/java/com/ellah/ellahveehotels/BookingActivity.java
+++ app/src/main/java/com/ellah/ellahveehotels/BookingActivity.java
@@ -2,13 +2,22 @@ package com.ellah.ellahveehotels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BookingActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        mLocationTextView = findViewById(R.id.locationTextView);
+
+        Intent intent = getIntent();
+        String location = getIntent().getStringExtra("location");
+        mLocationTextView.setText("Here are the hotels near " + location);
     }
 }
