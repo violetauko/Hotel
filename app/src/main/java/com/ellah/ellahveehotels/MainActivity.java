5d333463560a5463dc4c7687316de3271a99455c
violetauko <aukoviolet24@gmail.com>
2022-05-28 13:45:31 +0300

    add intent to start bookingActivity

--- app/src/main/java/com/ellah/ellahveehotels/MainActivity.java
+++ app/src/main/java/com/ellah/ellahveehotels/MainActivity.java
@@ -2,13 +2,26 @@ package com.ellah.ellahveehotels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mBookNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBookNow = findViewById(R.id.bookNow);
+        mBookNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BookingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
