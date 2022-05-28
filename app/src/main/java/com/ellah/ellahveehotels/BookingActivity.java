01f16cb1bb2657e5116982d666207dc7b83535b4
violetauko <aukoviolet24@gmail.com>
2022-05-28 18:59:28 +0300

    create intent for roomsActivity

--- app/src/main/java/com/ellah/ellahveehotels/BookingActivity.java
+++ app/src/main/java/com/ellah/ellahveehotels/BookingActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class BookingActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private Button mBook;
+    private Button mBook1;
+    private Button mBook2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,30 @@ public class BookingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_booking);
 
         mLocationTextView = findViewById(R.id.locationTextView);
-
+        mBook = findViewById(R.id.book);
+        mBook.setOnClickListener(new View.OnClickListener() {//set the onclick listener
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BookingActivity.this, RoomsActivity.class);//create an intent to open the RoomsActivity
+                startActivity(intent);//start the activity
+            }
+        });
+        mBook1 = findViewById(R.id.book1);
+        mBook1.setOnClickListener(new View.OnClickListener() {//set the onclick listener
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BookingActivity.this, RoomsActivity.class);//create an intent to open the RoomsActivity
+                startActivity(intent);//start the activity
+            }
+        });
+        mBook2 = findViewById(R.id.book2);
+        mBook2.setOnClickListener(new View.OnClickListener() {//set the onclick listener
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BookingActivity.this, RoomsActivity.class);//create an intent to open the RoomsActivity
+                startActivity(intent);//start the activity
+            }
+        });
         Intent intent = getIntent();
         String location = getIntent().getStringExtra("location");
         mLocationTextView.setText("Here are the hotels near " + location);
