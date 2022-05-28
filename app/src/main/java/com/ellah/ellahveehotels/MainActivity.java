b74febe590acc7a8862653a59d386fa1103a7ed3
violetauko <aukoviolet24@gmail.com>
2022-05-28 14:44:52 +0300

    add edit text to enter a location

--- app/src/main/java/com/ellah/ellahveehotels/MainActivity.java
+++ app/src/main/java/com/ellah/ellahveehotels/MainActivity.java
@@ -6,19 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindHotel;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText = findViewById(R.id.locationEditText);
         mFindHotel = findViewById(R.id.findHotel);//find the button in the activity_main.xml
         mFindHotel.setOnClickListener(new View.OnClickListener() {//set the onclick listener
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();//get the text from the edit text
                 Intent intent = new Intent(MainActivity.this, BookingActivity.class);//create an intent to open the BookingActivity
                 startActivity(intent);//start the activity
             }
