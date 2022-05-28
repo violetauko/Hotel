71509be9ef7dbb49e1c73ec5d16ecd90190db00c
violetauko <aukoviolet24@gmail.com>
2022-05-28 19:34:14 +0300

    add arrayAdapter to enable listing

--- app/src/main/java/com/ellah/ellahveehotels/RoomsActivity.java
+++ app/src/main/java/com/ellah/ellahveehotels/RoomsActivity.java
@@ -3,12 +3,31 @@ package com.ellah.ellahveehotels;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RoomsActivity extends AppCompatActivity {
-
+    private ListView mListView;
+    private String [] rooms = {"Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Room 6", "Room 7", "Room 8", "Room 9", "Room 10"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rooms);
+
+        mListView = findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, rooms);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(RoomsActivity.this, "You Clicked " + rooms[position] + " Great Choice", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
