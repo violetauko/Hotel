325d2d92168a06b4336ee813ad368f32bcef8e6c
violetauko <aukoviolet24@gmail.com>
2022-05-29 15:56:04 +0300

    introduce view.onClickListener interface

--- app/src/main/java/com/ellah/ellahveehotels/MainActivity.java
+++ app/src/main/java/com/ellah/ellahveehotels/MainActivity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.locationEditText) EditText mLocationEditText;//bind the locationEditText
     @BindView(R.id.findHotel) Button mFindHotel;//bind the findHotel button
 
@@ -21,14 +22,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);//bind the activity
 
-        mFindHotel.setOnClickListener(new View.OnClickListener() {//set the onclick listener
+        mFindHotel.setOnClickListener(this);
+    }//set the onclick listener
             @Override
             public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();//get the text from the edit text
-                Intent intent = new Intent(MainActivity.this, BookingActivity.class);//create an intent to open the BookingActivity
-                intent.putExtra("location", location);//put the location into the intent
-                startActivity(intent);//start the activity
+                if (v == mFindHotel) {
+                    String location = mLocationEditText.getText().toString();//get the location
+                    if (location.isEmpty()) {//if the location is empty
+                        Toast.makeText(MainActivity.this, "Please enter a location", Toast.LENGTH_SHORT).show();//show a toast
+                    } else {
+                        Intent intent = new Intent(MainActivity.this, BookingActivity.class);//create an intent to open the BookingActivity
+                        intent.putExtra("location", location);//put the location in the intent
+                        startActivity(intent);//start the activity
+                    }
+                }
             }
-        });
-    }
-}
+        }
