162a2c1e4ff46a16ef7d5c4bcc0165c4388e9581
violetauko <aukoviolet24@gmail.com>
2022-06-03 23:40:39 +0300

    create client class

--- app/src/main/java/com/ellah/ellahveehotels/BookingClient.java
+++ app/src/main/java/com/ellah/ellahveehotels/BookingClient.java
@@ -1,4 +1,43 @@
 package com.ellah.ellahveehotels;
 
-public interface BookingClient {
+import static com.ellah.ellahveehotels.Constants.BOOKING_API_KEY;
+import static com.ellah.ellahveehotels.Constants.BOOKING_BASE_URL;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class BookingClient {
+    private static  Retrofit retrofit = null;
+
+    public static BookingApi getClient() {
+
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest  = chain.request().newBuilder()
+                                    .addHeader("X-RapidAPI-Host", "booking-com.p.rapidapi.com")
+                                    .addHeader("X-RapidAPI-Key", BOOKING_API_KEY)
+                                    .build();
+                            return chain.proceed(newRequest);//proceeds with the request
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BOOKING_BASE_URL)//
+                    .client(okHttpClient)//adds the authorization header
+                    .addConverterFactory(GsonConverterFactory.create())//handles data serialization from JSON to Java objects
+                    .build();
+        }
+
+        return retrofit.create(BookingApi.class);
+    }
 }
--- app/src/main/java/com/ellah/ellahveehotels/Constants.java
+++ app/src/main/java/com/ellah/ellahveehotels/Constants.java
@@ -1,4 +1,4 @@
-﻿package com.ellah.ellahveehotels;
+package com.ellah.ellahveehotels;
 
 public class Constants {
     public static final String BOOKING_BASE_URL = "https://booking-com.p.rapidapi.com/v1/";
