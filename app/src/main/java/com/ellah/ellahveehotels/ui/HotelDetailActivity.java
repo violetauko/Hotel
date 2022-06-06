2ed7cb188a636be06bdf8d271d85880c14b879bf
joshuajoe12561@gmail.com <joshuajoe12561@gmail.com>
2022-06-06 19:12:42 +0300

    add details to hotelDetailActivity for display

--- app/src/main/java/com/ellah/ellahveehotels/ui/HotelDetailActivity.java
+++ app/src/main/java/com/ellah/ellahveehotels/ui/HotelDetailActivity.java
@@ -1,16 +1,39 @@
 package com.ellah.ellahveehotels.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.ellah.ellahveehotels.R;
+import com.ellah.ellahveehotels.adapters.HotelPagerAdapter;
+import com.ellah.ellahveehotels.models.Business;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HotelDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
+    private HotelPagerAdapter adapterViewPager;
+    List<Business> mHotels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel_detail);
+        ButterKnife.bind(this);
+
+        mHotels = Parcels.unwrap(getIntent().getParcelableExtra("hotels"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new HotelPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mHotels);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
