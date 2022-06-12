a036b06c8a2a9f3b2631f65323700b3d6b2c29cb
violetauko <aukoviolet24@gmail.com>
2022-06-12 19:33:05 +0300

    add createNewUser method to create new user

--- app/src/main/java/com/ellah/ellahveehotels/ui/CreateAccountActiviy.java
+++ app/src/main/java/com/ellah/ellahveehotels/ui/CreateAccountActiviy.java
@@ -1,20 +1,31 @@
 package com.ellah.ellahveehotels.ui;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ellah.ellahveehotels.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CreateAccountActiviy extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @BindView(R.id.createUserButton)
     Button mCreateUserButton;
     @BindView(R.id.nameEditText)
@@ -33,18 +44,56 @@ public class CreateAccountActiviy extends AppCompatActivity implements View.OnCl
 
         mCreateUserButton.setOnClickListener(this);
         mLoginTextView.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();//get firebase auth instance
+
+        createAuthStateListener();
+    }
+    private void createNewUser() {
+        //fetch the contents of registration form  and transform each value into a string
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+//call the built-in Firebase method createUserWithEmailAndPassword()
+// to create a new user account in Firebase,
+// passing in the user's email and password
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Authentication successful");
+                    } else {
+                        Toast.makeText(CreateAccountActiviy.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActiviy.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
     }
 
     @Override
     public void onClick(View view) {
         if (view == mLoginTextView) {
             Intent intent = new Intent(CreateAccountActiviy.this, LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);//manages back stack
             startActivity(intent);
             finish();
         }
         if (view == mCreateUserButton) {
-           createUser();
+            createNewUser();
         }
     }
 }
