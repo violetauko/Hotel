27b2b3bc53354e9f89d7117dc0032586104ec9d5
violetauko <aukoviolet24@gmail.com>
2022-06-12 18:06:09 +0300

    create createAccountActivity

---
+++ app/src/main/java/com/ellah/ellahveehotels/ui/CreateAccountActiviy.java
@@ -0,0 +1,16 @@
+package com.ellah.ellahveehotels.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ellah.ellahveehotels.R;
+
+public class CreateAccountActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_activiy);
+    }
+}
