ed248b93bcf34d5efdaf02f371e634569aef452b
violetauko <aukoviolet24@gmail.com>
2022-06-12 19:10:13 +0300

    add onclickLister on creatAccountActivity

--- app/src/main/java/com/ellah/ellahveehotels/ui/CreateAccountActiviy.java
+++ app/src/main/java/com/ellah/ellahveehotels/ui/CreateAccountActiviy.java
@@ -2,15 +2,49 @@ package com.ellah.ellahveehotels.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ellah.ellahveehotels.R;
 
-public class CreateAccountActiviy extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccountActiviy extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.createUserButton)
+    Button mCreateUserButton;
+    @BindView(R.id.nameEditText)
+    EditText mNameEditText;
+    @BindView(R.id.emailEditText)
+    EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_activiy);
+        ButterKnife.bind(this);
+
+        mCreateUserButton.setOnClickListener(this);
+        mLoginTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActiviy.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mCreateUserButton) {
+           createUser();
+        }
     }
 }
