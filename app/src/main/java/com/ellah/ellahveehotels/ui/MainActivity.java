44f7d72e3eaf4be9c7bd4fe6bfde47808b189c5d
violetauko <aukoviolet24@gmail.com>
2022-06-12 21:23:38 +0300

    add logout functionality

--- app/src/main/java/com/ellah/ellahveehotels/ui/MainActivity.java
+++ app/src/main/java/com/ellah/ellahveehotels/ui/MainActivity.java
@@ -8,6 +8,9 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +18,7 @@ import android.widget.Toast;
 
 import com.ellah.ellahveehotels.Constants;
 import com.ellah.ellahveehotels.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,68 +29,46 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-   // private SharedPreferences mSharedPreferences;
-    //private SharedPreferences.Editor mEditor;
-   // private DatabaseReference mSearchedLocationReference;
-   // private ValueEventListener mSearchedLocationReferenceListener;
 
     @BindView(R.id.findHotel) Button mFindHotel;//bind the findHotel button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        mSearchedLocationReference = FirebaseDatabase
-//                .getInstance()
-//                .getReference()
-//                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);//get the reference to the searchedLocation child in the database
-//
-//        mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                for (DataSnapshot locationSnapshot : snapshot.getChildren()) {
-//                    String location = locationSnapshot.getValue().toString();
-//                    Log.d("updated location", location);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {//if the database is unable to connect, display a toast message
-//
-//            }
-//        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);//bind the activity
-        //mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-       // mEditor = mSharedPreferences.edit();//get the editor
+
         mFindHotel.setOnClickListener(this);//set the onClickListener for the findHotel button
     }
     @Override
     public void onClick(View v) {
         if(v == mFindHotel) {
-            //String location = mLocationEditText.getText().toString();
-            //saveLocationToFirebase(location);
 
-//            if(!(location).equals("")) {
-//                addToSharedPreferences(location);
-//            }
             Intent intent = new Intent(MainActivity.this, BookingActivity.class);
-           // intent.putExtra("location", location);
             startActivity(intent);
         }
     }
-
-//    public void saveLocationToFirebase(String location) {
-//        mSearchedLocationReference.push().setValue(location);
-//    }
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);//remove the event listener when the activity is not in use
-//    }
-
-
-//    private void addToSharedPreferences(String location) {
-//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 }
