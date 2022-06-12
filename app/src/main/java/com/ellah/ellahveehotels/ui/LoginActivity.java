451b4a1b59fd3575a13ce407a0c92b46eff4edac
violetauko <aukoviolet24@gmail.com>
2022-06-12 18:11:56 +0300

    create LoginActivity

---
+++ app/src/main/java/com/ellah/ellahveehotels/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.ellah.ellahveehotels.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ellah.ellahveehotels.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
